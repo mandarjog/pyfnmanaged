49a7447cb32c27c15f3d7d03be49e3dfac6422bc
Mandar U Jog <mjog@plotwatt.com>
2015-10-15 04:18:44 -0400

    minor change

--- fnmanaged.py
+++ fnmanaged.py
@@ -12,7 +12,7 @@ In this case it will be affected by the FN_MANAGED env variable.
 
 It can turn readable sequential code into more performant code.
 The runtime decides how to best execute this piece of code.
-Look at the test to see what it can do
+Look at the tests to see what it can do
 """
 
 
